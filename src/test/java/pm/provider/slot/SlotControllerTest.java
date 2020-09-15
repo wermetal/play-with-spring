087b51a8b242f8b86201c1b7a00059c09825e934
AndriiG <andrii.golubenko@tain.com>
2020-09-15 10:45:51 +0300

    complete test for SlotController

--- src/test/java/pm/provider/slot/SlotControllerTest.java
+++ src/test/java/pm/provider/slot/SlotControllerTest.java
@@ -1,9 +1,10 @@
 package pm.provider.slot;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import pm.provider.integration.parimatch.player.PmPlayerInfo;
 import pm.provider.player.PlayerService;
 
@@ -30,28 +32,6 @@ public class SlotControllerTest {
     @MockBean
     private PlayerService playerService;
 
-
-    /*@BeforeEach
-    void init() {
-        PmPlayerInfo mockedPlayerInfo = new PmPlayerInfo("player1", 2000L, "UAH", "UA");
-        SpinResult spinResult = new SpinResult("round1", 300L, 1300L);
-        when(playerService.getPlayerInfo(any())).thenReturn(mockedPlayerInfo);
-        when(slotService.spin(any(), any(), any())).thenReturn(spinResult);
-    }*/
-
-    @Test
-    void whenValidInput_thenReturns200() throws Exception {
-        String sessionId = "sessionId";
-        SlotBetRequest betRequest = new SlotBetRequest(1000L);
-
-        mockMvc.perform(post("/slot/spin")
-                .contentType("application/json")
-                .header("Provider-Session-Id", sessionId)
-                .content(objectMapper.writeValueAsString(betRequest)))
-                .andExpect(status().isOk());
-
-    }
-
     @Test
     void whenNullValue_thenReturns400() throws Exception {
         String sessionId = "sessionId";
@@ -62,6 +42,49 @@ public class SlotControllerTest {
                 .header("Provider-Session-Id", sessionId)
                 .content(objectMapper.writeValueAsString(betRequest)))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void whenValidInput_thenMapsToBusinessModel() throws Exception {
+        String sessionId = "sessionId";
+        SlotBetRequest betRequest = new SlotBetRequest(1000L);
+
+        PmPlayerInfo mockedPlayerInfo = new PmPlayerInfo("player1", 2000L, "UAH", "UA");
+        when(playerService.getPlayerInfo(any())).thenReturn(mockedPlayerInfo);
+
+        mockMvc.perform(post("/slot/spin")
+                .contentType("application/json")
+                .header("Provider-Session-Id", sessionId)
+                .content(objectMapper.writeValueAsString(betRequest)))
+                .andExpect(status().isOk());
+
+        verify(playerService, times(1)).getPlayerInfo(eq(sessionId));
+        verify(slotService, times(1)).spin(
+                eq(mockedPlayerInfo),
+                eq(sessionId),
+                eq(betRequest.getBet())
+        );
+    }
+
+    @Test
+    void whenValidInput_thenReturnsSpinResult() throws Exception {
+        String sessionId = "sessionId";
+        SlotBetRequest betRequest = new SlotBetRequest(1000L);
+
+        SpinResult spinResult = new SpinResult("round1", 300L, 1300L);
+        when(slotService.spin(any(), any(), any())).thenReturn(spinResult);
+
+        MvcResult mvcResult = mockMvc.perform(post("/slot/spin")
+                .contentType("application/json")
+                .header("Provider-Session-Id", sessionId)
+                .content(objectMapper.writeValueAsString(betRequest)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String actualResponseBody = mvcResult.getResponse().getContentAsString();
+        assertThat(actualResponseBody).isEqualToIgnoringWhitespace(
+                objectMapper.writeValueAsString(spinResult));
+
     }
 
 }
