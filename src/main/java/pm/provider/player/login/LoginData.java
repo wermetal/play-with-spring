639299a97bc1408f685ce70968947bf5230b0663
AndriiG <andrii.golubenko@tain.com>
2020-09-15 15:14:49 +0300

    add login action test for PlayerController

--- src/main/java/pm/provider/player/login/LoginData.java
+++ src/main/java/pm/provider/player/login/LoginData.java
@@ -2,8 +2,20 @@ package pm.provider.player.login;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class LoginData {
+    @NotNull
     String login;
+
+    @NotNull
     String password;
+
+
+    public LoginData() {}
+    public LoginData(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
 }
---
+++ src/test/java/pm/provider/player/PlayerControllerTest.java
@@ -0,0 +1,63 @@
+package pm.provider.player;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import pm.provider.player.login.LoginData;
+
+
+@WebMvcTest(controllers = PlayerController.class)
+public class PlayerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PlayerService playerService;
+
+    @Test
+    void whenNullValue_thenLoginReturns400() throws Exception {
+        LoginData loginData = new LoginData(null, null);
+
+        mockMvc.perform(post("/player/login")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(loginData)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void whenValidInput_thenLoginMapsToBusinessModel() throws Exception {
+        String mockedSessionId = "session-string";
+        LoginData loginData = new LoginData("player1", "password");
+
+        when(playerService.login(any(), any())).thenReturn(mockedSessionId);
+
+        MvcResult mvcResult = mockMvc.perform(post("/player/login")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(loginData)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        verify(playerService, times(1)).login(
+                eq(loginData.getLogin()),
+                eq(loginData.getPassword())
+        );
+
+        String actualResponseBody = mvcResult.getResponse().getContentAsString();
+        assertThat(actualResponseBody).isEqualToIgnoringWhitespace(mockedSessionId);
+    }
+
+}
